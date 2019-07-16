eb9a323b8fbf5378699f1642a6d5d7dccc579aad
YuliaLee <yuli4ka_lee@gmail.com>
2019-07-16 12:22:30 +0300

    Transitions between pages added

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,10 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.should_be_login_link()      # проверяем наличие ссылки на логин
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)                          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                                             # открываем страницу
+    page.go_to_login_page()                                 # выполняем метод страницы - переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    login_page.should_be_login_page()                       # выполняем метод страницы - проверяем, корректна ли она
