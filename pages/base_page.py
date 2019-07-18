76d9cea8953ee180110d987897c53cc3467075a3
YuliaLee <yuli4ka_lee@gmail.com>
2019-07-18 11:59:30 +0300

    test_guest_can_add_product_to_cart added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 class BasePage(object):
     def __init__(self, browser, url, timeout=10):
@@ -15,3 +17,16 @@ class BasePage(object):
         except (NoSuchElementException):
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            print("Your code: {}".format(alert.text))
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,6 @@ class MainPageLocators(object):
 class LoginPageLocators(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators(object):
+    BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form>button.btn-add-to-basket")
